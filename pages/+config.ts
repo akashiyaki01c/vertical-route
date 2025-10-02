fe09cb10a005e9d3b0fbf19fd8178b0c1ae7926c
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2025-10-02 21:55:24 +0900

    fix: configを修正v2

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,3 +1,4 @@
+import type { Config } from "vike/types";
 import vikeReact from "vike-react/config";
 
 // Default config (can be overridden by pages)
@@ -6,7 +7,7 @@ import vikeReact from "vike-react/config";
 export default {
   // https://vike.dev/head-tags
   title: "縦断面図生成器",
-  base: "/virtical-route/",
+  baseServer: "/vertical-route/",
 
   extends: vikeReact,
-};
+} satisfies Config;
